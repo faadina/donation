bd098bf924d3737778b3f8449b78658ee26c9df6
faadina <syafairdina01@gmail.com>
2024-07-09 23:00:24 +0800

    MainHeader

---
+++ MainHeader.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="nav">
+            <ul>
+                <li><a href="MainHomepage.php">Home</a></li>
+                <li><a href="About.php">About</a></li>
+                <li><a href="MainContact.php">Contact</a></li>
+                <li><a href="MainLogin.php">Login</a></li>
+            </ul>
+    </div>
