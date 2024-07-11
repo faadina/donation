c84b2c5c0ecc8fd03f990f0f43b58518dbb77e5c
faadina <syafairdina01@gmail.com>
2024-07-11 15:59:43 +0800

    style

--- MainHeader.php
+++ MainHeader.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="initial-scale=1.0, width=device-width">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="donor/contact.css">
     <link rel="stylesheet" href="donor/bootstrap.min.css">
     <title><?php echo $title; ?></title>
     
--- MainHome.php
+++ MainHome.php
@@ -5,24 +5,23 @@ include 'MainHeader.php';
 
 <div class="main">
         <div class="main_content">
-            <h2>“Giving is not just about making a donation. It is about making a difference.”
+            <h2>Madrasah Tarbiyyah Islamiyyah Darul Hijrah
                 <br>
                 <span>Give a helping hand to children who NEED it!</span>
             </h2>
-            <div class="btn">
-                <a href="MainLogin.php">JOIN US TODAY</a>
+            <div class="button">
+                <a href="MainLogin.php">JOIN US</a>
             </div>
         </div>
     </div>
 
     <div class="side">
         <div class="left">
-            <img src="images/side.jpg">
+            <img src="images/mengajar.jpg">
         </div>
         <div class="right">
-            <h2>HELP A CHILD TODAY</h2>
             <p>
-                Service to man is service to God. Charity Jet cares for needy children by empowering their caregivers to do their best work, with compassion, grace, integrity and excellence. Our end goal is to support children worldwide and see every child reach the potential that God has for them.
+            Kompleks Anak Yatim Tahfiz Darul Hijrah is an independent educational institution established by Al Fadhil Ustaz Wan Muhammad Mizan bin Wan Abdul Latif in 2012. It was initiated to elevate an educational system based on the Quran and Sunnah, aiming to realize the aspiration of producing more Islamic scholars, particularly among orphans and the poor. This institution focuses on preventing dropouts and enabling these children to master the skills of memorizing, understanding the knowledge of the Quran, and comprehensively grasping Hadith.
             </p>
         </div>
 
