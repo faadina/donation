bf24141b51866a0ef3eb3cfb3c4186a63381e7cd
faadina <syafairdina01@gmail.com>
2026-05-09 09:27:46 +0800

    Fix database password variable name

--- dbConnect.php
+++ dbConnect.php
@@ -6,7 +6,7 @@ $databaseParts = $databaseUrl ? parse_url($databaseUrl) : [];
 
 $servername = getenv('MYSQLHOST') ?: getenv('MYSQL_HOST') ?: getenv('DB_HOST') ?: ($databaseParts['host'] ?? 'localhost');
 $username   = getenv('MYSQLUSER') ?: getenv('MYSQL_USER') ?: getenv('DB_USER') ?: ($databaseParts['user'] ?? 'root');
-$password   = getenv('MYSQLPASSWORD') ?: getenv('MYSQL_PASSWORD') ?: getenv('DB_PASS') ?: ($databaseParts['pass'] ?? '');
+$password   = getenv('MYSQLPASSWORD') ?: getenv('MYSQL_PASSWORD') ?: getenv('DB_PASSWORD') ?: getenv('DB_PASS') ?: ($databaseParts['pass'] ?? '');
 $dbname     = getenv('MYSQLDATABASE') ?: getenv('MYSQL_DATABASE') ?: getenv('DB_NAME') ?: (isset($databaseParts['path']) ? ltrim($databaseParts['path'], '/') : 'donationdb');
 $port       = (int)(getenv('MYSQLPORT') ?: getenv('MYSQL_PORT') ?: getenv('DB_PORT') ?: ($databaseParts['port'] ?? 3306));
 
